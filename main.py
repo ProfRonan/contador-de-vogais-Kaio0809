8d1b55b8fc35f2219603320ff8536444c4dce7da
Kaio <128155244+Kaio0809@users.noreply.github.com>
2023-05-03 13:52:31 +0000

    exercicio concluido

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-"""This program counts the number of vowels in a string."""
+#This program counts the number of vowels in a string.
 
 def count_vowels(string:str) -> int:
-    """
-    Takes in a string and returns the number of vowels in the string.
+    count = 0
+    for i in range(0, len(string)):
+        if string[i] == 'a' or string[i]== 'e' or string[i]== 'i' or string[i]== 'o' or string[i]== 'u' or string[i] == 'A' or string[i]== 'E' or string[i]== 'I' or string[i]== 'O' or string[i]== 'U':
+            count += 1
+    return(count)
 
-    Args:
-        string (str): The string to count vowels in.
-
-    Returns:
-        int: The number of vowels in the string.
-    """
+if __name__ == "__main__":
+    n = str(input('N: '))
+    print(count_vowels(n))
