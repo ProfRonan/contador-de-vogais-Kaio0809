213842a2eb74359a86cab624d0f14693bcebf75a
Kaio <128155244+Kaio0809@users.noreply.github.com>
2023-05-03 13:55:12 +0000

    exercicio concluido

--- main.py
+++ main.py
@@ -3,10 +3,10 @@
 def count_vowels(string:str) -> int:
     count = 0
     for i in range(0, len(string)):
-        if string[i] == 'a' or string[i]== 'e' or string[i]== 'i' or string[i]== 'o' or string[i]== 'u' or string[i] == 'A' or string[i]== 'E' or string[i]== 'I' or string[i]== 'O' or string[i]== 'U':
+        if string[i] == 'a' or string[i] == 'e' or string[i] == 'i' or string[i] == 'o' or string[i] == 'u' or string[i] == 'A' or string[i] == 'E' or string[i] == 'I' or string[i] == 'O' or string[i] == 'U':
             count += 1
     return(count)
 
 if __name__ == "__main__":
     n = str(input('N: '))
-    print(count_vowels(n))
+    print(count_vowels(n))   
